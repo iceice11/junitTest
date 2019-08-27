18f77d28507cb88d925b5b04b3b0060597b735e1
iceice11 <1291617594@qq.com>
2019-08-27 15:38:30 +0800

    commit again

--- src/main/java/com/fang/testNG/FirstTest.java
+++ src/main/java/com/fang/testNG/FirstTest.java
@@ -10,13 +10,14 @@ import org.testng.annotations.AfterClass;
 public class FirstTest {
   @Test
   public void f() {
+	  
+  }
+  @BeforeClass
+  public void beforeClass() {
 	  new App();
 	  Assert.assertEquals(App.add(1, 2), 3);
 	  Assert.assertEquals(App.add(2, 2), 4);
 	  Assert.assertEquals(App.add(1, 3), 4);
-  }
-  @BeforeClass
-  public void beforeClass() {
   }
 
   @AfterClass
