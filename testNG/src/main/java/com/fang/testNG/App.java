81676b815e4bcc66c003870868287e0176d95dd7
iceice11 <1291617594@qq.com>
2019-08-27 14:48:35 +0800

    second commit

---
+++ testNG/src/main/java/com/fang/testNG/App.java
@@ -0,0 +1,17 @@
+package com.fang.testNG;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+    
+    public static int add(int a, int b) {
+    	return a + b;
+    }
+}
---
+++ testNG/src/main/java/com/fang/testNG/FirstTest.java
@@ -0,0 +1,26 @@
+package com.fang.testNG;
+
+import org.testng.annotations.Test;
+
+import junit.framework.Assert;
+
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.AfterClass;
+
+public class FirstTest {
+  @Test
+  public void f() {
+	  new App();
+	  Assert.assertEquals(App.add(1, 2), 3);
+	  Assert.assertEquals(App.add(2, 2), 4);
+	  Assert.assertEquals(App.add(1, 3), 4);
+  }
+  @BeforeClass
+  public void beforeClass() {
+  }
+
+  @AfterClass
+  public void afterClass() {
+  }
+
+}
